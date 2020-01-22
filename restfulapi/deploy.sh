81e63e670472c26df30d0a1ce3995609065d38c8
pipitmnr <syafitri@alterra.id>
2020-01-22 14:08:12 +0700

    update deploysh

--- restfulapi/deploy.sh
+++ restfulapi/deploy.sh
@@ -7,7 +7,7 @@ git pull
 
 source ~/.profile
 echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
-docker stop hijub
-docker rm hijub
+docker stop hijub-deployment2
+docker rm hijub-deployment2
 docker rmi purnamasftr/hijub:deployment2
-docker run -d --name hijub -p 5000:5000 purnamasftr/hijub:deployment2
+docker run -d --name hijub-deployment2 -p 5000:5000 purnamasftr/hijub:deployment2
